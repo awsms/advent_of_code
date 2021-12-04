daf7be5d6303a74868b77353044c64d8f74125b5
awsms <x.a.z.x.e.e@protonmail.com>
2021-12-04 17:15:13 +0100

    solved day2

--- 21/02_p2.py
+++ 21/02_p2.py
@@ -3,3 +3,30 @@
 
 --- Part Two ---        '''
 
+import re
+
+z = []
+
+with open('02.txt', 'r') as file:
+    instructions = [i for i in file.readlines()]
+    for i in instructions:
+        z.append(re.split(' ', i))
+
+for i in range (len(z)):
+    z[i][1] = z[i][1].strip()
+    z[i][1] = int(z[i][1])
+
+aim,go_down,go_forward = 0,0,0
+
+for i, j in z:
+    if i == 'up':
+        aim -= j
+    if i == 'down':
+        aim += j
+    if i == 'forward':
+        go_forward += j
+        go_down += j * aim
+# print('go up:',go_up,'\ngo down:',go_down,'\ngo forward:',go_forward)
+# print('total:',(go_down-go_up)*go_forward)
+print('aim:',aim,'\ngo down:',go_down,'\ngo forward:',go_forward)
+print('final distance:',go_down*go_forward)
